7345477b58a3419d4066ab97e2ff0f0564926996
Kaleb Luhman <kaleb.luhman@gmail.com>
2020-04-05 16:03:13 -0500

    adds base type and extensions for firing events before/after saving changes

---
+++ src/EntityFrameworkCore.Extensions.Events/BaseDbContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkCore.Extensions.Events
+{
+    public abstract class BaseDbContext : DbContext
+    {
+        private readonly ICollection<IEventHandler> _eventHandlers;
+
+        protected BaseDbContext(IEnumerable<IEventHandler> eventHandlers)
+        {
+            _eventHandlers = eventHandlers.ToList();
+        }
+        
+        protected BaseDbContext(DbContextOptions options, ICollection<IEventHandler> eventHandlers) : base(options)
+        {
+            _eventHandlers = eventHandlers;
+        }
+
+        public override int SaveChanges()
+        {
+            return this.SaveChangesWithEvents(_eventHandlers, base.SaveChanges);
+        }
+
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            return this.SaveChangesWithEvents(acceptAllChangesOnSuccess, _eventHandlers, base.SaveChanges);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return this.SaveChangesWithEventsAsync(cancellationToken, _eventHandlers, base.SaveChangesAsync);
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            return this.SaveChangesWithEventsAsync(acceptAllChangesOnSuccess, cancellationToken, _eventHandlers, base.SaveChangesAsync);
+        }
+    }
+}
---
+++ src/EntityFrameworkCore.Extensions.Events/DbContextExtensions.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace EntityFrameworkCore.Extensions.Events
+{
+    public static class DbContextExtensions
+    {
+        public static int SaveChangesWithEvents(this DbContext context, ICollection<IEventHandler> eventHandlers, Func<int> saveChanges)
+        {
+            var entries = context.HandlePreSaveEvents(eventHandlers);
+            var result = saveChanges();
+            context.HandlePostSaveEvents(eventHandlers, entries);
+            
+            return result;
+        }
+        
+        public static int SaveChangesWithEvents(this DbContext context, bool acceptAllChangesOnSuccess, ICollection<IEventHandler> eventHandlers, Func<bool, int> saveChanges)
+        {
+            var entries = context.HandlePreSaveEvents(eventHandlers);
+            var result = saveChanges(acceptAllChangesOnSuccess);
+            context.HandlePostSaveEvents(eventHandlers, entries);
+            
+            return result;
+        }
+        
+        public static async Task<int> SaveChangesWithEventsAsync(this DbContext context, CancellationToken cancellationToken, ICollection<IEventHandler> eventHandlers, Func<CancellationToken, Task<int>> saveChanges)
+        {
+            var entries = context.HandlePreSaveEvents(eventHandlers);
+            var result = await saveChanges(cancellationToken);
+            context.HandlePostSaveEvents(eventHandlers, entries);
+            
+            return result;
+        }
+        
+        public static async Task<int> SaveChangesWithEventsAsync(this DbContext context, bool acceptAllChangesOnSuccess, CancellationToken cancellationToken, ICollection<IEventHandler> eventHandlers, Func<bool, CancellationToken, Task<int>> saveChanges)
+        {
+            var entries = context.HandlePreSaveEvents(eventHandlers);
+            var result = await saveChanges(acceptAllChangesOnSuccess, cancellationToken);
+            context.HandlePostSaveEvents(eventHandlers, entries);
+            
+            return result;
+        }
+        
+        private static Dictionary<EntityState, EntityEntry> HandlePreSaveEvents(this DbContext context, ICollection<IEventHandler> eventHandlers)
+        {
+            var entries = new Dictionary<EntityState, EntityEntry>();
+            foreach (var entry in context.ChangeTracker.Entries())
+            {
+                foreach (var eventHandler in eventHandlers)
+                {
+                    entries.Add(entry.State, entry);
+                    switch (entry.State)
+                    {
+                        case EntityState.Added:
+                            eventHandler.OnInserting(context, entry.Entity);
+                            break;
+                        case EntityState.Modified:
+                            eventHandler.OnUpdating(context, entry.OriginalValues.ToObject(), entry.Entity);
+                            break;
+                        case EntityState.Deleted:
+                            eventHandler.OnDeleting(context, entry.Entity);
+                            break;
+                    }
+                }
+            }
+
+            return entries;
+        }
+
+        private static void HandlePostSaveEvents(this DbContext context, ICollection<IEventHandler> eventHandlers, Dictionary<EntityState, EntityEntry> entries)
+        {
+            foreach (var (state, entry) in entries)
+            {
+                foreach (var eventHandler in eventHandlers)
+                {
+                    switch (state)
+                    {
+                        case EntityState.Added:
+                            eventHandler.OnInserted(context, entry.Entity);
+                            break;
+                        case EntityState.Modified:
+                            eventHandler.OnUpdated(context, entry.OriginalValues.ToObject(), entry.Entity);
+                            break;
+                        case EntityState.Deleted:
+                            eventHandler.OnDeleted(context, entry.Entity);
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
